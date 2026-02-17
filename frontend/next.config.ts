3409b38867b478c59af70ad14cbcae4ad2390ce3
Omoola003 <owolabigbolahan003@gmail.com>
2026-02-17 10:42:27 +0100

    feat: enhance deployment script and update frontend configuration for dashboard

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* This allows the app to run at http://54.154.74.37/dashboard */
+  basePath: '/dashboard',
+  
+  // Optional: If you use images from your own public folder, 
+  // they will now be at /dashboard/image.png
 };
 
 export default nextConfig;
